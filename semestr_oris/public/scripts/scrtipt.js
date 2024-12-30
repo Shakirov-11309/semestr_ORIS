e327573a568a57cd6f037c182b1497a7cc8b3700
Shakirov-11309 <ilham_sh2005@mail.ru>
2024-12-30 11:22:35 +0300

    [add] v1.4

--- semestr_oris/public/scripts/scrtipt.js
+++ semestr_oris/public/scripts/scrtipt.js
@@ -1,15 +1,39 @@
-const buttonFilter1 = document.getElementById("button-click-1");
-const imageByFilter1 = document.getElementById("imgClick1");
-
-buttonFilter1.addEventListener("click", (e) => {
-    buttonFilter1.click()
-    if(buttonFilter1.classList.contains("active"))
-    {
-        alert("Лох наелся блох")
-        imageByFilter1.src = "images/button-open.png";
+document.querySelector('.filter-left').addEventListener('click', function(event) {
+    if (event.target.id === 'button-click-1') {
+        const img = event.target.querySelector('.filter-image');
+        if(img.src = img.src.includes('button-close')){
+            img.src = 'images/button-open.png';
+            document.getElementsByClassName('filter-insert-category')[0].style.visibility = "visible";
+        }
+        else{
+            img.src = 'images/button-close.png';
+            document.getElementsByClassName('filter-insert-category')[0].style.visibility = "hidden";
+        }
     }
-    else
-    {
-        imageByFilter1.src = "images/button-close.png";
+});
+document.querySelector('.filter-left').addEventListener('click', function(event) {
+    if (event.target.id === 'button-click-2') {
+        const img = event.target.querySelector('.filter-image');
+        if(img.src = img.src.includes('button-close')){
+            img.src = 'images/button-open.png';
+            document.getElementsByClassName('filter-insert-category')[0].style.visibility = "visible";
+        }
+        else{
+            img.src = 'images/button-close.png';
+            document.getElementsByClassName('filter-insert-category')[0].style.visibility = "hidden";
+        }
     }
-})
+});
+document.querySelector('.filter-left').addEventListener('click', function(event) {
+    if (event.target.id === 'button-click-3') {
+        const img = event.target.querySelector('.filter-image');
+        if(img.src = img.src.includes('button-close')){
+            img.src = 'images/button-open.png';
+            document.getElementsByClassName('filter-insert-category')[0].style.visibility = "visible";
+        }
+        else{
+            img.src = 'images/button-close.png';
+            document.getElementsByClassName('filter-insert-category')[0].style.visibility = "hidden";
+        }
+    }
+});
